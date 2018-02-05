8b74fbd4fc68471fde2663ec46445f2eb600fd8c
Miro Guieb <miro.guieb@gmail.com>
2018-02-05 04:27:22 -0800

    Add interfaces

--- src/interface.d.ts
+++ src/interface.d.ts
@@ -1,1 +1,34 @@
-type PatientFormState = {}
+interface PatientFormState { }
+
+interface Ingredient {
+  id: number;
+  name: string;
+  minimum_percentage: number;
+  maximum_percentage: number;
+  description: string;
+  classes: string[];
+}
+
+interface FormulationIngredient {
+  id: number;
+  formulation_id: number;
+  ingredient_id: number;
+  percentage: number;
+}
+
+interface Formulation {
+  id: number;
+  name: string;
+  formulation_ingredients: FormulationIngredient[];
+}
+
+interface Patient {
+  name: string;
+  address: string;
+  dob: string;
+}
+
+interface PatientIngredient {
+  ingredient_id: number;
+  percentage: number;
+}
