b692dbe4d6e5d8a98e511a092b81d1cba7a81b14
Miro Guieb <miro.guieb@gmail.com>
2018-02-05 01:40:24 -0800

    Add Initial PatientForm

--- src/pages/PatientForm/index.tsx
+++ src/pages/PatientForm/index.tsx
@@ -1,13 +1,84 @@
 import * as React from 'react';
+import withStyles, { WithStyles, StyleRulesCallback } from 'material-ui/styles/withStyles';
+import Grid from 'material-ui/Grid';
+import TextField from 'material-ui/TextField';
 
-class PatientForm extends React.Component {
+import withRoot from '../../withRoot';
+
+const styles: StyleRulesCallback<'root' | 'container' | 'textField'> = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '100%'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+});
+
+class PatientForm extends React.Component<WithStyles<'root' | 'container' | 'textField'>, PatientFormState> {
+  state = {
+    name: '',
+    address: '',
+    dob: ''
+  };
+
+  handleChange = (name: string) => (event: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      [name]: event.currentTarget.value,
+    });
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="PatientForm">
-        <p>Patient Form</p>
-      </div>
+      <Grid container justify="center" spacing={0} className={classes.root}>
+        <Grid item xs={12} md={8}>
+          <form noValidate autoComplete="off" className={classes.container}>
+            <TextField
+              id="name"
+              label="Name"
+              className={classes.textField}
+              value={this.state.name}
+              onChange={this.handleChange('name')}
+              margin="normal"
+              fullWidth
+            />
+
+            <TextField
+              id="address"
+              label="Address"
+              className={classes.textField}
+              value={this.state.address}
+              onChange={this.handleChange('address')}
+              margin="normal"
+              fullWidth
+            />
+
+            <TextField
+              id="date"
+              label="Birthday"
+              className={classes.textField}
+              type="date"
+              value={this.state.dob}
+              onChange={this.handleChange('dob')}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+              fullWidth
+            />
+          </form>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default PatientForm;
+export default withRoot(withStyles(styles)<{}>(PatientForm));
