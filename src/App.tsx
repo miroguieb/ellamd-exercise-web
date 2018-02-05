f27a7264ec62c8ec128ddf961a1411ae65ecb427
Miro Guieb <miro.guieb@gmail.com>
2018-02-05 04:44:11 -0800

    Add app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
+import withStyles, { WithStyles } from 'material-ui/styles/withStyles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
+import withRoot from './withRoot';
 import PatientForm from './pages/PatientForm';
 
-class App extends React.Component {
+const styles = {
+  root: {
+    width: '100%',
+  },
+};
+
+class App extends React.Component<WithStyles<'root'>> {
   render() {
+    const { classes } = this.props;
+
     return (
-      <PatientForm />
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Patient Prescription Form
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <PatientForm />
+      </div>
     );
   }
 }
 
-export default App;
+export default withRoot(withStyles(styles)<{}>(App));
