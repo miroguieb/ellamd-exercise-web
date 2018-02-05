b786571276dac6c17afd5aa22f7eea59378dcfac
Miro Guieb <miro.guieb@gmail.com>
2018-02-04 19:56:28 -0800

    Add PatientForm page component

---
+++ src/pages/PatientForm/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class PatientForm extends React.Component {
+  render() {
+    return (
+      <div className="PatientForm">
+        <p>Patient Form</p>
+      </div>
+    );
+  }
+}
+
+export default PatientForm;
